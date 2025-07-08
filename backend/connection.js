ae7406d840edfe1fbb6f0a7b4e7664e922e3108b
Christin Joseph <christin.kjoseph@gmail.com>
2025-07-08 12:19:27 +0530

    changes done

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('').then(()=>{
+mongoose.connect('mongodb+srv://dbchris:chrisdb@cluster0.ylii4xb.mongodb.net/dbchris?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log("Connected to DB")
 }).catch(()=>{
     console.log("not connected");
